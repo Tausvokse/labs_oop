4bf6bd58471bbe774255526ab5bf51c9b53bc882
tausvokse <60479171+Tausvokse@users.noreply.github.com>
2023-02-14 17:18:06 +0200

    Add files via upload

---
+++ lab1.1/lab1.1_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using lab1._1_1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // створення змінних для подальшого утворенння класу
+        //A
+        Console.WriteLine("Enter xA");
+        float xA = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter yA");
+        float yA = float.Parse(Console.ReadLine());
+
+        //B
+        Console.WriteLine("Enter xB");
+        float xB = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter yB");
+        float yB = float.Parse(Console.ReadLine());
+
+        //створення об'єкта класу
+        Line line1 = new Line(xA, yA, xB, yB);
+        float P = line1.dovzhina();
+        float S = line1.kyt();
+        Console.WriteLine("A(" + xA + ";" + yA + ") and B(" + xB + ";" + yB + ")");
+        Console.WriteLine("Dovzhina: " + P);
+        Console.WriteLine("Kyt: " + S);
+    }
+}
---
+++ lab1.1/lab1.1_1/line.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._1_1
+{
+    class Line
+    {
+        private float xA, yA, xB, yB;
+
+        public Line(float xa, float ya, float xb, float yb)
+        {
+            this.xA = xa;
+            this.yA = ya;
+            this.xB = xb;
+            this.yB = yb;
+        }
+
+        public float dovzhina()
+        {
+             return (float)Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
+        }
+
+        public float kyt()
+        {
+            float vert = yB - yA;
+            float hor = xB - xA;
+            return (float)Math.Round(Math.Atan2(vert, hor)*(180/Math.PI), 2);
+        }
+    }
+}
