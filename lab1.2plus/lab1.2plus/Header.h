71714fbdae6363ef38444f2bfb91167108fc8c7b
Tausvokse <60479171+Tausvokse@users.noreply.github.com>
2023-02-27 14:33:29 +0200

    Add files via upload

---
+++ lab1.2plus/lab1.2plus/Header.h
@@ -0,0 +1,15 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+class line {
+public:
+	float xA, yA, xB, yB;
+	char choice;
+	int dia;
+	bool oper4;
+	float dovzhina();
+	float kyt();
+};
